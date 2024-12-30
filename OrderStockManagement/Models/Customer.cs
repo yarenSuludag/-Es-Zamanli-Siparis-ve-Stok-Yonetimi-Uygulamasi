79e2a966ecc304cf85c72993d45bf0230b4a4a8b
yarenSuludag <suludagy@gmail.com>
2024-12-30 21:20:11 +0300

    Güncellemeler yapıldı

--- OrderStockManagement/Models/Customer.cs
+++ OrderStockManagement/Models/Customer.cs
@@ -2,9 +2,27 @@
 {
     public class Customer
     {
+
+
+        private float _budget; // Private değişken
         public int CustomerID { get; set; }
         public string CustomerName { get; set; }
-        public float Budget { get; set; }
+        public float Budget
+        {
+            get => _budget;
+            set
+            {
+                // Budget sıfırın altına düşerse otomatik olarak sıfır yapılıyor
+                if (value < 0)
+                {
+                    _budget = 0;
+                }
+                else
+                {
+                    _budget = value;
+                }
+            }
+        }
         public string CustomerType { get; set; } // Premium or Standard
         public float TotalSpent { get; set; }
 
